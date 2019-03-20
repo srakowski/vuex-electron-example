b330d8e50fec2ddb111941217cea68f1212664d0
Shawn Rakowski <srakowski@live.com>
2019-03-19 19:06:26 -0500

    initial commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="wrapper">
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+  export default {
+    name: 'landing-page',
+  }
+</script>
+
+<style>
+</style>
---
+++ src/renderer/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+};
+
+const getters = {
+};
+
+const mutations = {
+};
+
+const actions = {
+};
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state,
+  getters,
+  mutations,
+  actions,
+})
